f7d1f4ea0f9d22ad9ee01f2f6caa240cb34fba5f
Magnus <mtry@kth.se>
2024-05-10 14:26:55 +0200

    Cerat a java class to handel exception

---
+++ src/main/java/se/mtry/iv1350/grocerystoreseminar/integration/ExceptionItemNotFound.java
@@ -0,0 +1,11 @@
+package se.mtry.iv1350.grocerystoreseminar.integration;
+
+/**
+ * error handling if not find the item.
+ * @author mtrys
+ */
+public class ExceptionItemNotFound {
+    
+}
+d
+        
